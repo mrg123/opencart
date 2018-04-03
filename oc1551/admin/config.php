5a51d1738fce96e3a50a0bac1c0f6b8fae002e32
mrg123 <ccna8008@163.com>
2018-04-03 10:03:26 +0800

    0

---
+++ oc1551/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc.localweb.com/oc1551/admin/');
+define('HTTP_CATALOG', 'http://oc.localweb.com/oc1551/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc.localweb.com/oc1551/admin/');
+define('HTTPS_CATALOG', 'http://oc.localweb.com/oc1551/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\git\opencart\oc1551/admin/');
+define('DIR_SYSTEM', 'D:\git\opencart\oc1551/system/');
+define('DIR_DATABASE', 'D:\git\opencart\oc1551/system/database/');
+define('DIR_LANGUAGE', 'D:\git\opencart\oc1551/admin/language/');
+define('DIR_TEMPLATE', 'D:\git\opencart\oc1551/admin/view/template/');
+define('DIR_CONFIG', 'D:\git\opencart\oc1551/system/config/');
+define('DIR_IMAGE', 'D:\git\opencart\oc1551/image/');
+define('DIR_CACHE', 'D:\git\opencart\oc1551/system/cache/');
+define('DIR_DOWNLOAD', 'D:\git\opencart\oc1551/download/');
+define('DIR_LOGS', 'D:\git\opencart\oc1551/system/logs/');
+define('DIR_CATALOG', 'D:\git\opencart\oc1551/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'oc1551');
+define('DB_PREFIX', 'oc_');
+?>
